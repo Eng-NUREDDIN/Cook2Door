2038012b62f0663edd92a1436836753372895612
radmanlo <radman.lotfiazar@gmail.com>
2023-07-09 01:34:22 +0300

    Routes are designed and implemeted, ready for implementing the controllers

--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
 const express = require('express')
-const db = require('./connection/db')
 require('dotenv').config()
+
+const db = require('./connection/db')
+const cookRoutes = require('./routes/cook')
+const customerRoutes = require('./routes/customer')
+const orderRoutes = require('./routes/order') 
 
 const app = express()
 
 const uri = process.env.DATABASE_URI
-db.connect(uri)
+const port = process.env.NODE_LOCAL_PORT;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use("/api/cook", cookRoutes);
+app.use("/api/customer", customerRoutes);
+app.use("/api/order", orderRoutes);
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`); 
+    db.connect(uri)
+});
+
+module.exports = app;
