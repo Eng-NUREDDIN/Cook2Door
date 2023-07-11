0056441fd464ee295a7014450f156eb22246b6f9
afrakucukaydin <afrakucukaydin@gmail.com>
2023-07-11 12:02:24 +0300

    Customer Controller Implemented #27

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -1,51 +1,95 @@
-/**
- * uncomment this after schema designed compeletly 
- * and pulled intpo the repository and delete this comment
- */
-// const customerSchema = require('../models/customerSchema')
+const customerSchema = require('../models/customerSchema');
 
 /**
- * This return all Customers 
+ * This returns all Customers
  * @param {*} req 
  * @param {*} res 
  */
-async function getAllCustomers (req, res) {
-
-    //implemant the code here
-
+async function getAllCustomers(req, res) {
+  try {
+    const customers = await customerSchema.find();
+    res.status(200).json(customers);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
 /**
- * Add new Customer into the Customer schema
+ * Add a new Customer to the Customer schema
  * @param {*} req 
  * @param {*} res 
  */
-async function addCustomer (req, res) {
+async function addCustomer(req, res) {
+  try {
+    // Extract customer data from the request body
+    const { customer_email, customer_name, customer_address, birthdate, provider, provider_id } = req.body;
+    
+    // Create a new customer instance
+    const customer = new customerSchema({
+      customer_email,
+      customer_name,
+      customer_address,
+      birthdate,
+      provider,
+      provider_id
+    });
 
-    //implemant the code here
-
+    // Save the customer to the database
+    const savedCustomer = await customer.save();
+    res.status(201).json(savedCustomer);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
 /**
- * Get Customer by its id
+ * Get a Customer by its id
  * @param {*} req 
  * @param {*} res 
  */
-async function getCustomerById (req, res) {
-
-    //implemant the code here
-
+async function getCustomerById(req, res) {
+  try {
+    // Extract customer id from the request parameters
+    const { id } = req.params;
+    
+    // Find the customer by id
+    const customer = await customerSchema.findById(id);
+    
+    // Check if customer exists
+    if (!customer) {
+      res.status(404).json({ message: "Customer not found" });
+      return;
+    }
+    
+    res.status(200).json(customer);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
 /**
- * remove the Customer based on the id
+ * Remove the Customer based on the id
  * @param {*} req 
  * @param {*} res 
  */
-async function removeCustomer (req, res) {
-
-    //implemant the code here
-
+async function removeCustomer(req, res) {
+  try {
+    // Extract customer id from the request parameters
+    const { id } = req.params;
+    
+    // Find and remove the customer by id
+    const removedCustomer = await customerSchema.findByIdAndRemove(id);
+    
+    // Check if customer exists
+    if (!removedCustomer) {
+      res.status(404).json({ message: "Customer not found" });
+      return;
+    }
+    
+    res.status(200).json({ message: "Customer removed successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
 /**
@@ -53,15 +97,44 @@ async function removeCustomer (req, res) {
  * @param {*} req 
  * @param {*} res 
  */
-async function updateCustomer (req, res) {
-
-    //implemant the code here
+async function updateCustomer(req, res) {
+  try {
+    // Extract customer id from the request parameters
+    const { id } = req.params;
     
+    // Extract updated customer data from the request body
+    const { customer_email, customer_name, customer_address, birthdate, provider, provider_id } = req.body;
+    
+    // Find and update the customer by id
+    const updatedCustomer = await customerSchema.findByIdAndUpdate(
+      id,
+      {
+        customer_email,
+        customer_name,
+        customer_address,
+        birthdate,
+        provider,
+        provider_id
+      },
+      { new: true }
+    );
+    
+    // Check if customer exists
+    if (!updatedCustomer) {
+      res.status(404).json({ message: "Customer not found" });
+      return;
+    }
+    
+    res.status(200).json(updatedCustomer);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
-module.exports = { getAllCustomers: getAllCustomers, 
-    addCustomer: addCustomer, 
-    getCustomerById: getCustomerById, 
-    removeCustomer: removeCustomer, 
-    updateCustomer: updateCustomer
-}
+module.exports = {
+  getAllCustomers,
+  addCustomer,
+  getCustomerById,
+  removeCustomer,
+  updateCustomer
+};
