9d6c9d394fcbbb0e5050e0a3b9a0efabf14789cb
talal <ta440237@gmail.com>
2023-07-13 14:42:32 +0300

    this customer_id and dish_id exist or not

--- src/models/orderSchema.js
+++ src/models/orderSchema.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema ({
-    dish_id: { type : String 
-       // type: mongoose.Schema.Types.ObjectId,
-       // ref: 'dishSchema'
+    dish_id: { 
+       type: mongoose.Schema.Types.ObjectId,
+       ref: 'dishSchema'
     },
-    customer_id: { type : String 
-       // type: mongoose.Schema.Types.ObjectId,
+    customer_id: { 
+       type: mongoose.Schema.Types.ObjectId,
         // changed it based on the name which is decided for cook Schema
-      //  ref: 'customerSchema'
+       ref: 'customerSchema'
     },
     order_description: {
         type: String,
