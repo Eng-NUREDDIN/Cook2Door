ecb10501d5e08c61800c66da9f6cea8d81089664
Eng-NUREDDIN <nour.hammoda@gmail.com>
2023-07-17 23:43:18 +0300

    singUp signIn Apis

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -23,6 +23,23 @@ async function getAllUsers(req, res) {
 async function signUp(req, res) {
   try {
     const { email, password, role } = req.body;
+    // Check for null or undefined values
+    if (email === null || email === undefined || password === null || password === undefined) {
+      res.status(400).json({ error: 'Email and password are required' });
+      return;
+    }
+
+    // Check for empty values (for strings)
+    if (email.trim() === '' || password.trim() === '') {
+      res.status(400).json({ error: 'Email and password cannot be empty' });
+      return;
+    }
+
+    // check for the role value 
+    if (role === null || role === undefined || role.trim() === '') {
+      res.status(400).json({ error: 'Role is required' });
+      return;
+    }
     const newUser = new userSchema({ email, password, role });
     await newUser.save();
     res.json({ message: 'User created successfully' });
@@ -39,6 +56,18 @@ async function signUp(req, res) {
 async function signIn(req, res) {
   try {
     const { email, password } = req.body;
+// Check for null or undefined values in email and password
+if (email === null || email === undefined || password === null || password === undefined) {
+  res.status(400).json({ error: 'Email and password are required' });
+  return;
+}
+
+// Check for empty values (for strings) in email and password
+if (email.trim() === '' || password.trim() === '') {
+  res.status(400).json({ error: 'Email and password cannot be empty' });
+  return;
+}
+     
     const user = await userSchema.findOne({ email, password });
 
     if (!user) {
@@ -61,6 +90,29 @@ async function updateUser(req, res) {
   try {
     const { id } = req.params;
     const updatedUser = req.body;
+        // Check for null or undefined values in email, password, and role
+        if (
+          updatedUser.email === null || updatedUser.email === undefined ||
+          updatedUser.password === null || updatedUser.password === undefined
+        ) {
+          res.status(400).json({ error: 'Email and password are required' });
+          return;
+        }
+    
+        // Check for empty values (for strings) in email, password, and role
+        if (
+          updatedUser.email.trim() === '' || updatedUser.password.trim() === '' ||
+          (updatedUser.role !== undefined && updatedUser.role.trim() === '')
+        ) {
+          res.status(400).json({ error: 'Email, password, and role cannot be empty' });
+          return;
+        }
+    
+        // check for the role value
+        if (updatedUser.role === null || updatedUser.role === undefined || updatedUser.role.trim() === '') {
+          res.status(400).json({ error: 'Role is required' });
+          return;
+        }
     await userSchema.findByIdAndUpdate(id, updatedUser);
     res.json({ message: 'User updated successfully' });
   } catch (error) {
